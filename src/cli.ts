70171bda0e8bdab1ed6c32b102c1d23a5f05e6d5
Mikael Vandmo <mikael@vandmo.se>
2025-05-02 18:33:47 +0000

    Make into docker image, part 4

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,48 @@
-import { readFileSync, writeFileSync } from "node:fs"
+import { mkdirSync, readFileSync, writeFileSync } from "node:fs"
 import { createSumType } from "./java-sumtype-generator.js"
+import { z } from "zod"
+import path from "node:path"
 
-const config = JSON.parse(readFileSync("java-sumtypes.json", "utf8"))
-
-writeFileSync(
-  "WithPackage.java",
-  createSumType({
-    packageName: "se.vandmo.javasumtypes",
-    name: "WithPackage",
-    types: {
-      Folder: {},
+/*
+{
+  "java-sumtypes": [
+    {
+      "name": "Tree",
+      "packageName": "se.vandmo.javasumtypes",
+      "types": {
+        "Empty": {},
+        "Leaf": { "value": "int" },
+        "Node": { "left": "Tree", "right": "Tree", "value": "int" }
+      },
+      "imports": "all"
     },
-  }),
-)
+*/
+
+const ConfigSchema = z.object({
+  "java-sumtypes": z.array(
+    z.object({
+      name: z.string(),
+      "package-name": z.optional(z.string()),
+      types: z.record(z.string(), z.record(z.string(), z.string())),
+      imports: z.enum(["all", "none"]).default("none"),
+    }),
+  ),
+})
+const config = ConfigSchema.parse(JSON.parse(readFileSync("java-sumtypes.json", "utf8")))
+if ("java-sumtypes" in config) {
+  for (const sumType of config["java-sumtypes"]) {
+    const packageName = sumType["package-name"]
+    const javaPath = path.join("src", "main", "java")
+    const packagePath = packageName ? path.join(javaPath, path.join(...packageName.split("."))) : javaPath
+    mkdirSync(packagePath, { recursive: true })
+    const filename = path.join(packagePath, `${sumType.name}.java`)
+    writeFileSync(
+      filename,
+      createSumType({
+        packageName,
+        name: sumType.name,
+        types: sumType.types,
+      }),
+    )
+  }
+}
